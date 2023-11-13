445d5c08c9dfb03e3cd76598622de11a87f7523a
SaifAbuR3d <saefraed2@gmail.com>
2023-11-13 21:10:21 +0200

    Setup the SignalRServer

---
+++ SignalRServer/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace SignalRServer;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        CreateWebHostBuilder(args).Build().Run();
+    }
+
+    private static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>();
+
+}
