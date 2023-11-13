ea6b543d47aaed061dda16a19a0dfcd2b4b549a7
SaifAbuR3d <saefraed2@gmail.com>
2023-11-13 21:23:30 +0200

    Add SignalR Event Consumer Receives alerts and prints to console

---
+++ SignalRAlertConsumer/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Configuration;
+
+namespace SignalRAlertConsumer;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var builder = new ConfigurationBuilder()
+              .SetBasePath(Directory.GetCurrentDirectory())
+              .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+        var configuration = builder.Build();
+
+        var connection = ConnectToSignalRServer(configuration);
+
+
+        connection.On("ReceiveAlert", (string alert) => { Console.WriteLine(alert); });
+
+
+        Console.ReadLine();
+    }
+
+    private static HubConnection ConnectToSignalRServer(IConfigurationRoot configuration)
+    {
+        string signalRHubUrl = configuration.GetConnectionString("SignalRHubUrl");
+
+        var connection = new HubConnectionBuilder()
+            .WithUrl(signalRHubUrl)
+            .Build();
+
+        try
+        {
+            connection.StartAsync().Wait();
+            Console.WriteLine("SignalR connected.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"SignalR connection error: {ex.Message}");
+        }
+
+        return connection;
+    }
+}
