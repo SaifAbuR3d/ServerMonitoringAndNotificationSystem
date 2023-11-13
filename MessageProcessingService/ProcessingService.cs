17154c38ccd568f8cd655ea449782e4e5fab1ceb
SaifAbuR3d <saefraed2@gmail.com>
2023-11-13 21:41:07 +0200

    Add ProcessingService class

---
+++ MessageProcessingService/ProcessingService.cs
@@ -0,0 +1,34 @@
+﻿namespace MessageProcessingService;
+
+public class ProcessingService
+{
+    private readonly IMessageQueueReceiver _messageQueueReceiver;
+    private readonly IMongoRepository _mongoRepository;
+    private readonly NotificationManager _notificationManager;
+
+    public ProcessingService(
+        IMessageQueueReceiver messageQueueReceiver,
+        IMongoRepository mongoRepository,
+        NotificationManager notificationManager)
+    {
+        _messageQueueReceiver = messageQueueReceiver;
+        _mongoRepository = mongoRepository;
+        _notificationManager = notificationManager;
+    }
+
+    public void Start()
+    {
+        _notificationManager.Start();
+        _messageQueueReceiver.StartReceiving(HandleServerStatistics);
+    }
+
+    private void HandleServerStatistics(StatisticsReceived statistics)
+    {
+        var previousStatistics = _mongoRepository.GetLastRecord(); 
+        _notificationManager.CheckAlerts(statistics, previousStatistics); 
+
+        Console.WriteLine(statistics);
+
+        _mongoRepository.Insert(statistics);
+    }
+}
