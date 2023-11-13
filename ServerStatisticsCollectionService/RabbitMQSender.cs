3baf73ef802f780c4e1c082ef0e7919d35e9bf5d
SaifAbuR3d <saefraed2@gmail.com>
2023-11-13 20:59:38 +0200

    implement RabbitMQSender

---
+++ ServerStatisticsCollectionService/RabbitMQSender.cs
@@ -0,0 +1,56 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+namespace ServerStatisticsCollectionService;
+
+public class RabbitMQSender : IMessageQueueSender, IDisposable
+{
+    private readonly IConnection _connection;
+    private readonly IModel _channel;
+
+    public RabbitMQSender(string hostName, int port, string userName, string password)
+    {
+        var factory = new ConnectionFactory
+        {
+            HostName = hostName,
+            Port = port,
+            UserName = userName,
+            Password = password
+        };
+        factory.ClientProvidedName = "Server StatisticsSent Collector Service";
+
+        _connection = factory.CreateConnection();
+        _channel = _connection.CreateModel();
+
+    }
+
+    public void Publish(string topic, StatisticsSent message)
+    {
+        string exchangeName = "Topic Exchange";
+        string queueName = $"Topic Queue";
+
+        _channel.ExchangeDeclare(exchangeName, ExchangeType.Topic);
+        _channel.QueueDeclare(queueName, durable: false, exclusive: false, autoDelete: false);
+        _channel.QueueBind(queueName, exchangeName, topic);
+
+
+        string jsonMessage = JsonSerializer.Serialize(message);
+        var bytesMessage = Encoding.UTF8.GetBytes(jsonMessage);
+
+        _channel.BasicPublish(
+            exchange: exchangeName,
+            routingKey: topic,
+            basicProperties: null,
+            body: bytesMessage
+        );
+
+
+    }
+
+    public void Dispose()
+    {
+        _channel?.Close();
+        _connection?.Close();
+    }
+}
