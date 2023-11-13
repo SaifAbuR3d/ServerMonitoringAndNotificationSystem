7993374b0dffdf2585ca6324ca577e62443829b9
SaifAbuR3d <saefraed2@gmail.com>
2023-11-13 20:57:52 +0200

    Add StatisticsSent Class

---
+++ ServerStatisticsCollectionService/StatisticsSent.cs
@@ -0,0 +1,14 @@
+﻿namespace ServerStatisticsCollectionService;
+
+public class StatisticsSent
+{
+    public double MemoryUsage { get; set; }
+    public double AvailableMemory { get; set; }
+    public double CpuUsagePercentage { get; set; }
+    public DateTime Timestamp { get; set; }
+
+    public override string ToString()
+    {
+        return $"{{Memory Usage={Math.Round(MemoryUsage, 2)}MB, Available Memory={AvailableMemory}MB, Cpu Usage={Math.Round(CpuUsagePercentage, 2)}%, Time={Timestamp}}}";
+    }
+}
