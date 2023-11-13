997320113441e4a7092c6f7106d29d30edebb928
SaifAbuR3d <saefraed2@gmail.com>
2023-11-13 21:30:33 +0200

    Add StatisticsReceived class

---
+++ MessageProcessingService/StatisticsReceived.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace MessageProcessingService;
+
+[BsonIgnoreExtraElements]
+public class StatisticsReceived
+{
+    public string ServerIdentifier { get; set; }
+    public double MemoryUsage { get; set; }
+    public double AvailableMemory { get; set; }
+    public double CpuUsagePercentage { get; set; }
+    public DateTime Timestamp { get; set; }
+    public double MemoryUsagePercentage => MemoryUsage / (MemoryUsage + AvailableMemory) * 100;
+
+    public override string ToString()
+    {
+        return $"{{Memory Usage={Math.Round(MemoryUsage, 2)}MB, Available Memory={AvailableMemory}MB, Memory Usage Percentage={Math.Round(MemoryUsagePercentage, 2)}%, Cpu Usage Percentage={Math.Round(CpuUsagePercentage, 2)}%, Time={Timestamp}}}";
+    }
+}
