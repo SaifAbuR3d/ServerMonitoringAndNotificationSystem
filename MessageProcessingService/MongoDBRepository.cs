30fd785e8361938165eef761ffb3389fd0dae908
SaifAbuR3d <saefraed2@gmail.com>
2023-11-13 21:35:38 +0200

    Implement MongoDBRepository

---
+++ MessageProcessingService/MongoDBRepository.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Driver;
+
+namespace MessageProcessingService;
+
+public class MongoDBRepository : IMongoRepository
+{
+    private readonly IMongoCollection<StatisticsReceived> _collection;
+
+    public MongoDBRepository(string connectionString)
+    {
+        string databaseName = "MessageProcessingService";
+        string collectionName = "ServerStatisticsInfo";
+
+        var client = new MongoClient(connectionString);
+        var database = client.GetDatabase(databaseName);
+        _collection = database.GetCollection<StatisticsReceived>(collectionName);
+    }
+
+    public void Insert(StatisticsReceived statistics)
+    {
+        _collection.InsertOne(statistics);
+    }
+
+    public StatisticsReceived? GetLastRecord()
+    {
+        return _collection.Find(_ => true).SortByDescending(e => e.Timestamp).FirstOrDefault();
+    }
+}
